7fad319c4daa3c2278ba18272d630fb4bd4bdcfa
PolliBu <8polli.bu@gmail.com>
2024-02-28 18:03:23 +0200

    add page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import ContactList from '../components/ContactList/ContactList';
-import ContactForm from '../components/ContactForm/ContactForm';
-import { fetchContacts } from '../redux/contacts/operations';
-import { selectLoading } from '../redux/contacts/selectors';
-import SearchBox from '../components/SearchBox/SearchBox';
-import { selectError } from '../redux/contacts/selectors';
-import Title from '../components/Title/Title';
+import ContactList from '../../components/ContactList/ContactList';
+import ContactForm from '../../components/ContactForm/ContactForm';
+import { fetchContacts } from '../../redux/contacts/operations';
+import { selectLoading } from '../../redux/contacts/selectors';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import { selectError } from '../../redux/contacts/selectors';
+import Title from '../../components/Title/Title';
 
 export default function Tasks() {
   const dispatch = useDispatch();
