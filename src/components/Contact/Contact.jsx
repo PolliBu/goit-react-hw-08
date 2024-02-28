83f9d9d1a78164b70f7fe1f5ad078029121c82c9
PolliBu <8polli.bu@gmail.com>
2024-02-28 20:42:35 +0200

    Modal from 'react-modal'

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,10 +3,39 @@ import { FaPhone } from 'react-icons/fa6';
 import css from './Contact.module.css';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/operations';
+import Modal from 'react-modal';
+import React from 'react';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 const Contact = ({ contact: { name, number, id } }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteContact(id));
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div className={css.contact}>
@@ -18,9 +47,21 @@ const Contact = ({ contact: { name, number, id } }) => {
           <FaPhone /> {number}
         </p>
       </div>
-      <button className={css.btn} onClick={handleDelete}>
+      <button className={css.btn} onClick={openModal}>
         Delete
       </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={_subtitle => (subtitle = _subtitle)}>
+          Do you really want to delete this contact?
+        </h2>
+        <button onClick={handleDelete}>Yes, delete!</button>
+      </Modal>
     </div>
   );
 };
